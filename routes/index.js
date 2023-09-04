8544d0713207a6f572168a764a6c0ec2aa5c6644
Mdlalose4life <blessing1059@gmail.com>
2023-09-04 22:51:55 +0200

    /user routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 function controllerRouting(app){
   const router = express.Router();
@@ -11,6 +12,22 @@ function controllerRouting(app){
 
   router.get('/stats', (req, res) => {
     AppController.getStastus(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res)
+  });
+
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res)
+  });
+
+  router.get('/users/me', (req, res) => {
+    UserController.getMe(req, res)
   });
 }
 export default controllerRouting;
