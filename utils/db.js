2d72fb31c0339b036abca8a779371816839f109b
Mdlalose4life <blessing1059@gmail.com>
2023-09-04 16:18:48 +0200

    db

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,51 @@
 const { MongoClient } = require('mongodb');
 
+const DB_HOST = process.env.DB_HOST || localhost
+const DB_PORT = process.env.DB_PORT || 27017
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager'
+const url = `mongodb://${DB_HOST}:${DB_PORT}`
+
+/**
+ * 
+ */
 class DBClient{
-    constructor(){
-        
+  constructor(){
+    MongoClient.connect(url, (err, client) =>{
+      if(!err){
+          this.db = client.db(DB_DATABASE);
+          this.users = this.db.collection('users');
+          this.files = this.db.collection('files');
+        } else {
+            console.log(err.message);
+            this.db = false;
+        }
+      });
     }
-}
+    /**
+     * 
+     * @returns 
+     */
+    isAlive(){
+      return Boolean(this.db)
+    }
+
+    /**
+     * 
+     * @returns 
+     */
+    async nbUsers(){
+      numberOfUsers = this.users.countDocuments();
+      return numberofUsers
+    }
+
+    /**
+     * 
+     * @returns 
+     */
+    async nbFiles(){
+      return numberOfFiles = this.files.countDocuments();
+    }
+  }
+  const dbClient = new DBClient();
+
+  export default dbClient;
